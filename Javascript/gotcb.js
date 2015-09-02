0783f53d1b0a9d76eb2234adc83307617978897c
Lisa Kilker <lisakilker@gmail.com>
2015-09-02 19:30:21 +0200

    new

--- Javascript/gotcb.js
+++ Javascript/gotcb.js
@@ -3,24 +3,10 @@ function fileActions(err, file){
         throw err;
     }
     var episodes = JSON.parse(file)
-    console.log(episodes[0].title)
+    console.log("Title:" + " " + episodes[0].title + " " + "Episode#:" + episodes[0].episode_number);
+    console.log(episodes[0].description)
+    console.log("Rating" + " " + episodes[0].rating)
 }
 
 module.exports = fileActions
-
-// function fileactions(err, file){ 
-    
-// 	var str = '{"title": "description": "rating": "episode_number"}';
-//     var episodes = JSON.parse(file)
-//     	if (err) {
-//         throw err;
-//     }
-
-// 	} 	else {
-// 			var str = "";
-// 		for (var i = 0; i<lines.length; i++) {
-// 		console.log(episodes[0].title) 
-// 	}
-// }
-
 
